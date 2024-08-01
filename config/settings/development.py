2221fb0f4ed3d110d69a4a9ac1782834e79846a0
Yash Sonalia <sonaliayash@gmail.com>
2024-08-01 12:05:01 +0530

    (update) ALLOWED_HOSTS

--- config/settings/development.py
+++ config/settings/development.py
@@ -1,7 +1,7 @@
 from .base import *
 
 DEBUG = True
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", '*']
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
